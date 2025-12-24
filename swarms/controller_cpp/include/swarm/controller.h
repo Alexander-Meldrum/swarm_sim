c3f5b6c4f137ca2c5d07044fea8008957793325c
Alexander Meldrum <Alexander-Meldrum@users.noreply.github.com>
2025-12-24 16:36:27 +0100

    Improve Controller

---
+++ swarms/controller_cpp/include/swarm/controller.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <grpcpp/grpcpp.h>
+#include <swarm.pb.h> // in build folder
+#include <swarm.grpc.pb.h> // in build folder
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using swarm_proto::swarm_proto_service;
+using swarm_proto::StepRequest;
+using swarm_proto::StepResponse;
+using swarm_proto::ResetRequest;
+using swarm_proto::ResetResponse;
+using swarm_proto::DroneAction;
+using swarm_proto::DroneObservation;
+
+namespace swarm {
+
+class SwarmController {
+public:
+    explicit SwarmController(std::unique_ptr<swarm_proto_service::Stub> stub)
+        : stub_(std::move(stub)) {}
+
+
+    ResetResponse reset(uint32_t num_drones, uint64_t max_steps, float dt);
+    StepResponse step();
+
+    
+
+    const std::vector<float>& observations_() const {return obs_;}
+    
+
+private:
+    std::unique_ptr<swarm_proto_service::Stub> stub_;
+    uint32_t num_drones_{0};
+    uint64_t step_{0};
+    float dt_{0.0};
+    // std::vector<swarm_proto::DroneObservation> obs_;
+    // Flat observation vector
+    std::vector<float> obs_;
+    uint8_t num_observation_features_ = 3;
+
+    // Called internally by reset() and step()
+    void consume_observations_from_proto(const google::protobuf::RepeatedPtrField<swarm_proto::DroneObservation>& observations);
+
+};
+
+} // namespace swarm
---
+++ swarms/controller_cpp/src/controller.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <grpcpp/grpcpp.h>
+#include "proto/swarm.pb.h"
+#include "proto/swarm.grpc.pb.h"
+#include "swarm/controller.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using swarm_proto::swarm_proto_service;
+using swarm_proto::StepRequest;
+using swarm_proto::StepResponse;
+using swarm_proto::ResetRequest;
+using swarm_proto::ResetResponse;
+using swarm_proto::DroneAction;
+using swarm_proto::DroneObservation;
+
+namespace swarm {
+
+void SwarmController::consume_observations_from_proto(const google::protobuf::RepeatedPtrField<swarm_proto::DroneObservation>& observations)
+{
+    obs_.clear();
+    for (const auto& o : observations) {
+        obs_.push_back(o.ox());
+        obs_.push_back(o.oy());
+        obs_.push_back(o.oz());
+        obs_.push_back(o.collision_count());
+        // obs_.push_back(o.vx());
+        // obs_.push_back(o.vy());
+        // obs_.push_back(o.vz());
+    }
+}
+
+ResetResponse SwarmController::reset(uint32_t num_drones, uint64_t max_steps, float dt) {
+    ResetRequest request;
+    ResetResponse response;
+    grpc::ClientContext context;
+    
+    // Data that we request to be set in simulator
+    request.set_num_drones(num_drones);
+    request.set_max_steps(max_steps);
+    request.set_dt(dt);
+
+    // BLOCKING RPC call (synchronous)
+    auto status = stub_->Reset(&context, request, &response);
+    if (!status.ok()) {
+        throw std::runtime_error(status.error_message());
+    }
+    // Populate data that authoritative simulator responds with
+    num_drones_ = response.num_drones();
+    step_ = response.step();
+    dt_ = response.dt();
+
+    // Observations saved until next step
+    obs_.reserve(num_drones_ * num_observation_features_);
+    SwarmController::consume_observations_from_proto(response.observations());
+
+    std::cout << "Simulator reset to step: " << step_ << std::endl;
+    return response;
+}
+
+StepResponse SwarmController::step() {
+    StepRequest request;
+    StepResponse response;
+    grpc::ClientContext context;
+    
+    request.set_step(step_);
+
+    // TODO, Actions depend on observations
+
+    // Build actions (one per drone)
+    for (uint32_t i = 0; i < num_drones_; ++i) {
+        DroneAction* a = request.add_actions();
+        a->set_ax(0.1f);
+        a->set_ay(0.0f);
+        a->set_az(0.0f);
+    }
+
+    // BLOCKING RPC call (synchronous)
+    auto status = stub_->Step(&context, request, &response);
+    if (!status.ok()) {
+        throw std::runtime_error(status.error_message());
+    }
+
+    step_ = response.step();
+
+    if (response.done()) {
+        // TODO
+        std::cout << "World Simulator Done" << std::endl;
+        // Stop stepping immediately
+        // Log episode return
+        return response;
+    }
+
+    // Consume observations, save for next step
+    SwarmController::consume_observations_from_proto(response.observations());
+
+    float reward = response.reward();
+    std::cout   << "Step:    " << step_
+                << "reward:  " << reward
+                << std::endl;
+
+
+    return response;
+}
+
+} // namespace swarm
